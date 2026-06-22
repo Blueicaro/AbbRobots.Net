e8fff3f29fbaecf45a7bcc51382950526f50fe69
Jorge Turiel <jorge.turiel@gmail.com>
2026-06-22 15:00:19 +0200

    Fix bug reading one line

--- src/AbbRobots.Net/Parser/EioParser.cs
+++ src/AbbRobots.Net/Parser/EioParser.cs
@@ -7,28 +7,54 @@ namespace AbbRobots.Net.Parser
     {
         //Lista para guardar las señales
         public List<string> LoadedSignals{get; private set;}=new List<string>
-        public void TextParser(string rawText)
+        public void ParseText(string rawText)
+{
+    LoadedSignals.Clear();
+
+    string[] lines = rawText.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+    
+    // To store the lines
+    string accumulatedLine = "";
+
+    foreach (string line in lines)
+    {
+        string cleanLine = line.Trim();
+
+        // Skip empty lines or comments, but WATCH OUT: 
+//      // Only if we are not accumulating a split line."
+        if (string.IsNullOrEmpty(cleanLine) || cleanLine.StartsWith("#"))
         {
-            LoadedSignals.Clear();
-            string[] lines = rawText.Split(new[] {"\r\n","\r","\n"},StringSplitOptions.None);
-
-            foreach (string line in lines)
-            {
-                //Remove white spaces at begin and End
-                string cleanLine = line.Trim();
-                
-                //Skip empty lines and coments
-                if ((string.IsNullOrWhiteSpace(cleanLine)) || cleanLine.StartsWith("#")){
-                    continue;
-                }
-
-                if (cleanLine.StartsWith("-Name"))
-                {
-                    LoadedSignals.Add(cleanLine);
-                }
-
-            }
-
+            continue;
         }
+
+        // If we got info we add it
+        if (!string.IsNullOrEmpty(accumulatedLine))
+        {
+            accumulatedLine += " " + cleanLine;
+        }
+        else
+        {
+            accumulatedLine = cleanLine;
+        }
+
+        // This line finish with  '\'?)
+        if (accumulatedLine.EndsWith("\\"))
+        {
+            // Clean the slash at the end
+            accumulatedLine = accumulatedLine.Substring(0, accumulatedLine.Length - 1).Trim();
+            
+            // Jump to the next file
+            continue; 
+        }
+  
+        if (accumulatedLine.StartsWith("-Name"))
+        {
+            LoadedSignals.Add(accumulatedLine);
+        }
+
+        // Clean for next line
+        accumulatedLine = "";
+    }
+}
     }
 }
